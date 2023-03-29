6d9a2199142bd50a20824f3492a9eb7033dd0d64
zekurom <61953862+zekurom@users.noreply.github.com>
2023-03-29 10:09:51 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,58 @@
 import createBareServer from "@tomphttp/bare-server-node";
 import express from "express";
+import express from "express";
+const router = express.Router()
 import { createServer } from "node:http";
-import { publicPath } from "ultraviolet-static";
+//import { publicPath } from "ultraviolet-static";
 import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
-import { join } from "node:path";
+import { join, resolve } from "node:path";
+import { readFile } from 'fs/promises';
 import { hostname } from "node:os";
+import basicAuth from 'express-basic-auth'
+const publicPath = resolve('./src/static/public')
 
 const bare = createBareServer("/bare/");
 const app = express();
+
+const json = JSON.parse(
+  await readFile(
+    new URL('./users.json', import.meta.url)
+  )
+);
+console.log(json)
+app.use(basicAuth({
+    users: json,
+	challenge: true,
+	authorizer: myAuthorizer,
+    unauthorizedResponse: getUnauthorizedResponse	
+}))
+
+function myAuthorizer(username, password) {
+	let keys  = Object.keys(json)
+	// iterate over each element in the array
+	for (var i = 0; i < keys.length; i++){
+	  // look for the entry with a matching `code` value
+		const userMatches = basicAuth.safeCompare(username, keys[i])
+		if (userMatches){
+			// we found it
+	    	// obj[i].name is the matched result
+			const passwordMatches = basicAuth.safeCompare(password, json[username])
+			if (userMatches && passwordMatches){
+				// console.log(username, "  ", password, " has logged in")
+			}
+    		return userMatches & passwordMatches
+		}
+	}
+    
+}
+
+
+
+function getUnauthorizedResponse(req) {
+    return req.auth
+        ? ('Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected')
+        : 'No credentials provided'
+}
 
 // Load our publicPath first and prioritize it over UV.
 app.use(express.static(publicPath));
@@ -69,6 +114,13 @@ function shutdown() {
     process.exit(0);
   });
 }
+
+//SOCKETIO
+//import { Server } from "socket.io";
+//const io = new Server(server);
+//io.on('connection', (socket) => {
+//  console.log('a user connected');
+//});
 
 server.listen({
   port,
