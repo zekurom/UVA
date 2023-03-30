e5c7c03e5a4edf5a8ee824acd6a985c6c6b2d16a
zekurom <61953862+zekurom@users.noreply.github.com>
2023-03-30 10:10:40 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,8 +16,6 @@ const app = express();
 console.log("Starting server")
 
 console.log("Retrieving public IP...")
-var http = require('http');
-
 get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
   resp.on('data', function(ip) {
     console.log("My public IP address is: " + ip);
